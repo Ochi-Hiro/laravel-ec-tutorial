402031819590eb38df764eb1ca6a1cde636aa11e
Ochiai Hiroki <ontooff.ontooff@gmail.com>
2023-04-16 02:38:12 +0900

    BladeComponentStady

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class ComponentTestController extends Controller
 {
     public function showComponent1(){
-        return view('umarche_tes.component-test1');
+        $message = 'コントローラーで設定した変数を渡す message';
+        return view('umarche_tes.component-test1',
+        compact('message'));
     }
 
     public function showComponent2(){
--- resources/views/umarche_tes/component-test1.blade.php
+++ resources/views/umarche_tes/component-test1.blade.php
@@ -1,1 +1,14 @@
-コンポーネントテスト１
+<x-umarche_comp_blade_tes.app>
+
+    <h1>bladeファイルとcomponentについて</h1>
+    <br>
+    <x-slot name="header_tes">
+        名前付きスロット(header_tes)の文字
+    <x-slot>
+    
+    ブレードファイルの文字    
+
+    <x-umarche_comp_blade_tes.card title="titleに渡された文字 タイトル" content="content属性に渡された文字 本文" :message="$message" />
+    <x-umarche_comp_blade_tes.card title="titleに渡された文字 タイトル2" />
+    <x-umarche_comp_blade_tes.card title="CSSを変更したい" class="bg-red-300"/>
+</x-umarche_comp_blade_tes.app>
