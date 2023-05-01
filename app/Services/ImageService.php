e7a611c45b19cef81c76ab0e3e7da161e4b3442e
Ochiai Hiroki <ontooff.ontooff@gmail.com>
2023-05-01 18:39:02 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -8,11 +8,19 @@ use InterventionImage;
 class ImageService
 {
     public static function upload($imageFile, $folderName) 
-    {    
+    {
+        // dd($imageFile);
+        if(is_array($imageFile)) // 配列か判定し、それぞれ$fileに格納する
+        {
+            $file = $imageFile['image'];
+        } else {
+            $file = $imageFile;
+        }
+
         $fileName = uniqid(rand().'_');
-        $extension = $imageFile->extension(); //拡張子を取得する
+        $extension = $file->extension(); //拡張子を取得する
         $fileNameToStore = $fileName . '.' . $extension;
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+        $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
         //InterventionImage::make($imageFile)でInterventionImageで使えるようになる。リサイズしてencodeで画像として扱うことができる。
         Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
         //第1引数にフォルダ場所からのファイル名を指定。第2引数にリサイズした画像を指定する。
