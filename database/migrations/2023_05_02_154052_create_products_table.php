0ff553a50455b2afd7fba868150a1c5938f16873
Ochiai Hiroki <ontooff.ontooff@gmail.com>
2023-05-04 18:11:51 +0900

    product create

--- database/migrations/2023_05_02_154052_create_products_table.php
+++ database/migrations/2023_05_02_154052_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id')
             ->constrained()
             ->onUpdate('cascade')
@@ -20,6 +25,15 @@ return new class extends Migration
             $table->foreignId('secondary_category_id')
             ->constrained(); //caregoryは消さないのでcascade不要
             $table->foreignId('image1')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image2')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image3')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image4')
             ->nullable()
             ->constrained('images');
             //('~_id')だとlaravelでModelを推測してくれるが、違う場合は判別できない為constrainedの中で指定する。null許可はconstrainedの前に。
