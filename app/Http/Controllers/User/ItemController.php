d5b073b162c81aaa5d0811062485ed0fe3281b3c
shiro-kuma <120717898+shiro-kuma92@users.noreply.github.com>
2023-05-15 07:39:48 +0900

    constractor edit

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -13,6 +13,17 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('item');
+            if(!is_null($id)){
+                $itemId = Product::availableItems()->where('products.id', $id)->exists(); //existsで入ってきたidがwhereの結果で存在しているか確認して入っていたらtrue返す
+                if(!$itemId){
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
     }
     
     public function index()
