a2b4d7c0a1a2a22eb347d4ec73a5ae82b6b26217
shiro-kuma <120717898+shiro-kuma92@users.noreply.github.com>
2023-05-16 08:15:29 +0900

    index form

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use App\Models\PrimaryCategory;
 
 class ItemController extends Controller
 {
@@ -28,11 +29,17 @@ class ItemController extends Controller
     
     public function index(Request $request)
     {
+        // dd($request);
+
+        $categories = PrimaryCategory::with('secondary') //EagerLoading
+        ->get();
+
         $products = Product::availableItems() //商品一覧の取得 scope
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
         
-        return view('user.index', compact('products'));
+        return view('user.index', 
+        compact('products', 'categories'));
     }
 
     public function show($id)
